2922c84ff46abdd8fa405eb2f5beb5916c5872b8
Thomas Winsnes <thomas@ideadrought.com>
2025-02-13 22:06:41 +1100

    updated config path to fix issue with loading dags

--- dags/dbt/dbt_dag.py
+++ dags/dbt/dbt_dag.py
@@ -15,7 +15,7 @@ profile_config = ProfileConfig(
 )
 
 dbt_snowflake_dag = DbtDag(
-    project_config=ProjectConfig("/usr/local/airflow/dags/dbt/dbt_test",),
+    project_config=ProjectConfig("/opt/airflow/dags/repo/dags/dbt/dbt_test",),
     operator_args={"install_deps": True},
     profile_config=profile_config,
     execution_config=ExecutionConfig(dbt_executable_path=f"{os.environ['AIRFLOW_HOME']}/dbt_venv/bin/dbt",),
